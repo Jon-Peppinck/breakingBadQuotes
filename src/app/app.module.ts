f38d500386cb388e3d2a17d42dc24c049d3cc6b8
Jon Peppinck <mrjonpeppinck@gmail.com>
2019-01-28 13:05:48 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/Forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { RandomQuoteComponent } from './components/random-quote/random-quote.component';
+import { DiscussionComponent } from './components/discussion/discussion.component';
+import { AllPostsComponent } from './components/all-posts/all-posts.component';
+import { PostComponent } from './components/post/post.component';
+
+import { CharactersService } from './services/characters.service';
+import { QuoteService } from './services/quote.service';
+import { AllPostsService } from './services/all-posts.service';
+import { FourZeroFourComponent } from './components/four-zero-four/four-zero-four.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    LandingPageComponent,
+    RandomQuoteComponent,
+    DiscussionComponent,
+    AllPostsComponent,
+    PostComponent,
+    FourZeroFourComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  providers: [CharactersService, QuoteService, AllPostsService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
